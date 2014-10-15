4c468529ff6717f02721f8775d1b6f6fcf1fc498
hikalkan <hi_kalkan@yahoo.com>
2014-10-15 19:55:21 +0300

    Revert "Upgraded version and ABP package references."
    This reverts commit b32e413e16f1e06b494f8c7135d3ee925b0c9c84.

--- src/Abp.Zero/Zero/AbpZeroModule.cs
+++ src/Abp.Zero/Zero/AbpZeroModule.cs
@@ -11,7 +11,7 @@ namespace Abp.Zero
         /// <summary>
         /// Current version of the zero module.
         /// </summary>
-        public const string CurrentVersion = "0.2.1.5";
+        public const string CurrentVersion = "0.2.1.4";
 
         public override void Initialize()
         {
