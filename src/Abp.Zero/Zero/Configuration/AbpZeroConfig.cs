d6127a1127cbd916d60ddab712b0d5a71c0dff24
hikalkan <hi_kalkan@yahoo.com>
2015-03-21 22:23:30 +0200

    Added missing AbpZeroConfig file..

---
+++ src/Abp.Zero/Zero/Configuration/AbpZeroConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Abp.Zero.Configuration
+{
+    internal class AbpZeroConfig : IAbpZeroConfig
+    {
+        public IUserManagementConfig UserManagement
+        {
+            get { return _userManagementConfig; }
+        }
+        private readonly IUserManagementConfig _userManagementConfig;
+
+        public AbpZeroConfig(IUserManagementConfig userManagementConfig)
+        {
+            _userManagementConfig = userManagementConfig;
+        }
+    }
+}
