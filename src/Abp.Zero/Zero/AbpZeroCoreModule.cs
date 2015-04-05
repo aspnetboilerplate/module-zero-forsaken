e9f5c980ab73bd0f7b90120b29b9ff2fd7564585
hikalkan <hi_kalkan@yahoo.com>
2015-04-05 19:24:42 +0300

    Incremented version.

--- src/Abp.Zero/Zero/AbpZeroCoreModule.cs
+++ src/Abp.Zero/Zero/AbpZeroCoreModule.cs
@@ -12,7 +12,7 @@ namespace Abp.Zero
         /// <summary>
         /// Current version of the zero module.
         /// </summary>
-        public const string CurrentVersion = "0.5.13.1";
+        public const string CurrentVersion = "0.5.14.0";
 
         public override void PreInitialize()
         {
