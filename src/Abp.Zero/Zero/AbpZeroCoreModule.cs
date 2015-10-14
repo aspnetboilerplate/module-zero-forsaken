462825df48efc79b5e50386ef79d06b9c9a3ee9d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-10-14 19:25:02 +0300

    Upgraded to ABP 0.7.3 and inherited AbpSession from ClaimsAbpSession by renaming it to IdentityFrameworkClaimsAbpSession.

--- src/Abp.Zero/Zero/AbpZeroCoreModule.cs
+++ src/Abp.Zero/Zero/AbpZeroCoreModule.cs
@@ -14,7 +14,7 @@ namespace Abp.Zero
         /// <summary>
         /// Current version of the zero module.
         /// </summary>
-        public const string CurrentVersion = "0.7.2.0";
+        public const string CurrentVersion = "0.7.3.0";
 
         public override void PreInitialize()
         {
