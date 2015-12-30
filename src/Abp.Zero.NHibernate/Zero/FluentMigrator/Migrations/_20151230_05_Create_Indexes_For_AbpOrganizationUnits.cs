100f94267b23484c9832a760fa0d73d6c1e45690
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-12-30 17:19:39 +0200

    Created indexes for Permissions and OrganizationUnits.

---
+++ src/Abp.Zero.NHibernate/Zero/FluentMigrator/Migrations/_20151230_05_Create_Indexes_For_AbpOrganizationUnits.cs
@@ -0,0 +1,23 @@
+using FluentMigrator;
+
+namespace Abp.Zero.FluentMigrator.Migrations
+{
+    [Migration(2015123005)]
+    public class _20151230_05_Create_Indexes_For_AbpOrganizationUnits : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Index("IX_TenantId_Code")
+                .OnTable("AbpOrganizationUnits")
+                .OnColumn("TenantId").Ascending()
+                .OnColumn("Code").Ascending()
+                .WithOptions().NonClustered();
+
+            Create.Index("IX_TenantId_ParentId")
+                .OnTable("AbpOrganizationUnits")
+                .OnColumn("TenantId").Ascending()
+                .OnColumn("ParentId").Ascending()
+                .WithOptions().NonClustered();
+        }
+    }
+}
