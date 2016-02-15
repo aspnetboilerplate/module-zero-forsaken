13c3eb73f8ed8b9446d1604f310c7160e1068a03
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-02-15 11:42:47 +0200

    Upgraded to ABP 0.8.

--- src/Abp.Zero/Zero/AbpZeroCoreModule.cs
+++ src/Abp.Zero/Zero/AbpZeroCoreModule.cs
@@ -17,7 +17,7 @@ namespace Abp.Zero
         /// <summary>
         /// Current version of the zero module.
         /// </summary>
-        public const string CurrentVersion = "0.7.7.1";
+        public const string CurrentVersion = "0.8.0.0";
 
         public override void PreInitialize()
         {
