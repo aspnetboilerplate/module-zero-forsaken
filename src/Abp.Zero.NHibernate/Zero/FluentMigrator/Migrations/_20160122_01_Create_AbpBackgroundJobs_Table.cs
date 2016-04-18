d156089362939e03e210bc45e756b3a506d0acc6
sonkey2000 <ljm_garfield@qq.com>
2016-04-18 17:34:24 +0800

    Update _20160122_01_Create_AbpBackgroundJobs_Table.cs

--- src/Abp.Zero.NHibernate/Zero/FluentMigrator/Migrations/_20160122_01_Create_AbpBackgroundJobs_Table.cs
+++ src/Abp.Zero.NHibernate/Zero/FluentMigrator/Migrations/_20160122_01_Create_AbpBackgroundJobs_Table.cs
@@ -1,13 +1,15 @@
+using Abp.FluentMigrator.Extensions;
 using FluentMigrator;
 
 namespace Abp.Zero.FluentMigrator.Migrations
 {
-    [Migration(20160122)]
+    [Migration(2016012201)]
     public class _20160122_01_Create_AbpBackgroundJobs_Table : AutoReversingMigration
     {
         public override void Up()
         {
             Create.Table("AbpBackgroundJobs")
+                .WithIdAsInt32()
                 .WithColumn("JobType").AsString(512).NotNullable()
                 .WithColumn("JobArgs").AsString(1048576).NotNullable()
                 .WithColumn("TryCount").AsInt16().NotNullable().WithDefaultValue(0)
@@ -18,7 +20,7 @@ namespace Abp.Zero.FluentMigrator.Migrations
                 .WithCreationAuditColumns();
 
             Create.Index("IX_IsAbandoned_NextTryTime")
-                .OnTable("AbpUsers")
+                .OnTable("AbpBackgroundJobs")
                 .OnColumn("IsAbandoned").Ascending()
                 .OnColumn("NextTryTime").Ascending()
                 .WithOptions().NonClustered();
