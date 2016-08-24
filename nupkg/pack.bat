fc3c6c49ad770c53b4fa606e57fbc91f54e33c03
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-08-24 13:34:05 +0300

    Added EF Core packaging to batch file.

--- nupkg/pack.bat
+++ nupkg/pack.bat
@@ -6,6 +6,7 @@ SET /P VERSION_SUFFIX=Please enter version-suffix (can be left empty):
 dotnet "pack" "..\src\Abp.Zero" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 dotnet "pack" "..\src\Abp.Zero.Ldap" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 dotnet "pack" "..\src\Abp.Zero.EntityFramework" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
+dotnet "pack" "..\src\Abp.Zero.EntityFrameworkCore" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 dotnet "pack" "..\src\Abp.Zero.NHibernate" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 
 pause
