df47eab5239f2d8b5b6cb0b92245f48b1bcd2eea
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-09-28 13:39:38 +0300

    Resolved #255: Added Abp.Zero.Owin to packages.

--- nupkg/pack.bat
+++ nupkg/pack.bat
@@ -4,6 +4,7 @@ REM "..\tools\gitlink\GitLink.exe" ..\ -u https://github.com/aspnetboilerplate/m
 SET /P VERSION_SUFFIX=Please enter version-suffix (can be left empty): 
 
 dotnet "pack" "..\src\Abp.Zero" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
+dotnet "pack" "..\src\Abp.Zero.Owin" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 dotnet "pack" "..\src\Abp.Zero.Ldap" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 dotnet "pack" "..\src\Abp.Zero.EntityFramework" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
 dotnet "pack" "..\src\Abp.Zero.EntityFrameworkCore" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
