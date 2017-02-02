e215c30cfca66f4cd12dd99d329087a4e69f12cf
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2017-02-02 15:18:11 +0300

    resolved #293

--- test/Abp.Zero.SampleApp.Tests/TestDatas/InitialUsersBuilder.cs
+++ test/Abp.Zero.SampleApp.Tests/TestDatas/InitialUsersBuilder.cs
@@ -27,12 +27,23 @@ namespace Abp.Zero.SampleApp.Tests.TestDatas
             var admin = _context.Users.Add(
                 new User
                 {
-                   TenantId = defaultTenant.Id,
-                   Name = "System",
-                   Surname = "Administrator",
-                   UserName = User.AdminUserName,
-                   Password = new PasswordHasher().HashPassword("123qwe"),
-                   EmailAddress = "admin@aspnetboilerplate.com"
+                    TenantId = defaultTenant.Id,
+                    Name = "System",
+                    Surname = "Administrator",
+                    UserName = User.AdminUserName,
+                    Password = new PasswordHasher().HashPassword("123qwe"),
+                    EmailAddress = "admin@aspnetboilerplate.com"
+                });
+
+            _context.Users.Add(
+                new User
+                {
+                    TenantId = defaultTenant.Id,
+                    Name = "System",
+                    Surname = "Manager",
+                    UserName = "manager",
+                    Password = new PasswordHasher().HashPassword("123qwe"),
+                    EmailAddress = "manager@aspnetboilerplate.com"
                 });
         }
     }
