ee188ed6a91fc13cfa605f30c91d3c2a37999292
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-03-09 10:01:25 +0300

    Added appveyor.yml

---
+++ nupkg/pack.ps1
@@ -0,0 +1,37 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$slnPath = Join-Path $packFolder "../"
+$srcPath = Join-Path $slnPath "src"
+
+# List of projects
+$projects = (
+    "Abp.Zero",
+    "Abp.Zero.Owin",
+    "Abp.Zero.AspNetCore",
+    "Abp.Zero.Ldap",
+    "Abp.Zero.EntityFramework",
+    "Abp.Zero.EntityFrameworkCore",
+    "Abp.Zero.NHibernate"
+)
+
+# Rebuild solution
+Set-Location $slnPath
+& dotnet msbuild /t:Rebuild /p:Configuration=Release
+
+# Copy all nuget packages to the pack folder
+foreach($project in $projects) {
+    
+    $projectFolder = Join-Path $srcPath $project
+
+    # Create nuget pack
+    Set-Location $projectFolder
+    & dotnet msbuild /t:pack /p:Configuration=Release /p:IncludeSymbols=true
+
+    # Copy nuget package
+    $projectPackPath = Join-Path $projectFolder ("/bin/Release/" + $project + ".*.nupkg")
+    Move-Item $projectPackPath $packFolder
+
+}
+
+# Go back to the pack folder
+Set-Location $packFolder
