f6355988fee8e2bed4ce25cae1911244ce7d334c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-03-15 13:44:08 +0300

    Refactor AbpUser and AbpUserBase.

--- src/Abp.Zero.Common/Authorization/Users/AbpUserBase.cs
+++ src/Abp.Zero.Common/Authorization/Users/AbpUserBase.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Configuration;
 using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
+using Abp.Extensions;
 
 namespace Abp.Authorization.Users
 {
@@ -37,6 +40,32 @@ namespace Abp.Authorization.Users
         /// </summary>
         public const int MaxAuthenticationSourceLength = 64;
 
+        /// <summary>
+        /// UserName of the admin.
+        /// admin can not be deleted and UserName of the admin can not be changed.
+        /// </summary>
+        public const string AdminUserName = "admin";
+
+        /// <summary>
+        /// Maximum length of the <see cref="Password"/> property.
+        /// </summary>
+        public const int MaxPasswordLength = 128;
+
+        /// <summary>
+        /// Maximum length of the <see cref="Password"/> without hashed.
+        /// </summary>
+        public const int MaxPlainPasswordLength = 32;
+
+        /// <summary>
+        /// Maximum length of the <see cref="EmailConfirmationCode"/> property.
+        /// </summary>
+        public const int MaxEmailConfirmationCodeLength = 328;
+
+        /// <summary>
+        /// Maximum length of the <see cref="PasswordResetCode"/> property.
+        /// </summary>
+        public const int MaxPasswordResetCodeLength = 328;
+        
         /// <summary>
         /// Authorization source name.
         /// It's set to external authentication source name if created by an external source.
@@ -87,6 +116,92 @@ namespace Abp.Authorization.Users
         public virtual string FullName { get { return this.Name + " " + this.Surname; } }
 
         /// <summary>
+        /// Password of the user.
+        /// </summary>
+        [Required]
+        [StringLength(MaxPasswordLength)]
+        public virtual string Password { get; set; }
+
+        /// <summary>
+        /// Confirmation code for email.
+        /// </summary>
+        [StringLength(MaxEmailConfirmationCodeLength)]
+        public virtual string EmailConfirmationCode { get; set; }
+
+        /// <summary>
+        /// Reset code for password.
+        /// It's not valid if it's null.
+        /// It's for one usage and must be set to null after reset.
+        /// </summary>
+        [StringLength(MaxPasswordResetCodeLength)]
+        public virtual string PasswordResetCode { get; set; }
+
+        /// <summary>
+        /// Lockout end date.
+        /// </summary>
+        public virtual DateTime? LockoutEndDateUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the access failed count.
+        /// </summary>
+        public virtual int AccessFailedCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lockout enabled.
+        /// </summary>
+        public virtual bool IsLockoutEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        public virtual string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Is the <see cref="PhoneNumber"/> confirmed.
+        /// </summary>
+        public virtual bool IsPhoneNumberConfirmed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the security stamp.
+        /// </summary>
+        public virtual string SecurityStamp { get; set; }
+
+        /// <summary>
+        /// Is two factor auth enabled.
+        /// </summary>
+        public virtual bool IsTwoFactorEnabled { get; set; }
+
+        /// <summary>
+        /// Login definitions for this user.
+        /// </summary>
+        [ForeignKey("UserId")]
+        public virtual ICollection<UserLogin> Logins { get; set; }
+
+        /// <summary>
+        /// Roles of this user.
+        /// </summary>
+        [ForeignKey("UserId")]
+        public virtual ICollection<UserRole> Roles { get; set; }
+
+        /// <summary>
+        /// Claims of this user.
+        /// </summary>
+        [ForeignKey("UserId")]
+        public virtual ICollection<UserClaim> Claims { get; set; }
+
+        /// <summary>
+        /// Permission definitions for this user.
+        /// </summary>
+        [ForeignKey("UserId")]
+        public virtual ICollection<UserPermissionSetting> Permissions { get; set; }
+
+        /// <summary>
+        /// Settings for this user.
+        /// </summary>
+        [ForeignKey("UserId")]
+        public virtual ICollection<Setting> Settings { get; set; }
+
+        /// <summary>
         /// Is the <see cref="AbpUserBase.EmailAddress"/> confirmed.
         /// </summary>
         public virtual bool IsEmailConfirmed { get; set; }
@@ -102,6 +217,23 @@ namespace Abp.Authorization.Users
         /// </summary>
         public virtual DateTime? LastLoginTime { get; set; }
 
+        protected AbpUserBase()
+        {
+            IsActive = true;
+            IsLockoutEnabled = true;
+            SecurityStamp = SequentialGuidGenerator.Instance.Create().ToString();
+        }
+
+        public virtual void SetNewPasswordResetCode()
+        {
+            PasswordResetCode = Guid.NewGuid().ToString("N").Truncate(MaxPasswordResetCodeLength);
+        }
+
+        public virtual void SetNewEmailConfirmationCode()
+        {
+            EmailConfirmationCode = Guid.NewGuid().ToString("N").Truncate(MaxEmailConfirmationCodeLength);
+        }
+
         /// <summary>
         /// Creates <see cref="UserIdentifier"/> from this User.
         /// </summary>
@@ -109,6 +241,11 @@ namespace Abp.Authorization.Users
         public virtual UserIdentifier ToUserIdentifier()
         {
             return new UserIdentifier(TenantId, Id);
+        }
+
+        public override string ToString()
+        {
+            return $"[User {Id}] {UserName}";
         }
     }
 }
