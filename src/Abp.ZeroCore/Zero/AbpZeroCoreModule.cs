a39fc96b1a5c95b9cbf43ea768e6c9e4195f8af6
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-03-15 13:47:32 +0300

    Add AbpZeroCoreModule #323

---
+++ src/Abp.ZeroCore/Zero/AbpZeroCoreModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Abp.Modules;
+
+namespace Abp.Zero
+{
+    [DependsOn(typeof(AbpZeroCommonModule))]
+    public class AbpZeroCoreModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
