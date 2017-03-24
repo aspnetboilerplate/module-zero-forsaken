33820657d6efb714ef4ae18a7e39ee347f3a1da2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-03-24 16:12:23 +0300

    Moved some localizations to it's own project.

--- src/Abp.Zero/Zero/AbpZeroCoreModule.cs
+++ src/Abp.Zero/Zero/AbpZeroCoreModule.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using Abp.Localization.Dictionaries.Xml;
+using Abp.Localization.Sources;
 using Abp.Modules;
 
 namespace Abp.Zero
@@ -6,6 +8,18 @@ namespace Abp.Zero
     [DependsOn(typeof(AbpZeroCommonModule))]
     public class AbpZeroCoreModule : AbpModule
     {
+        public override void PreInitialize()
+        {
+            Configuration.Localization.Sources.Extensions.Add(
+                new LocalizationSourceExtensionInfo(
+                    AbpZeroConsts.LocalizationSourceName,
+                    new XmlEmbeddedFileLocalizationDictionaryProvider(
+                        Assembly.GetExecutingAssembly(), "Abp.Zero.Localization.SourceExt"
+                    )
+                )
+            );
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
