ca95aa2379fc7e983352aca417b4dbaea8295626
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-05-25 15:29:00 +0300

    Update JwtSecurityTokenHandler claims mappings. Also add AbpIdentityServerOptions.

--- src/Abp.ZeroCore.IdentityServer4/IdentityServer4/AbpZeroIdentityServerBuilderExtensions.cs
+++ src/Abp.ZeroCore.IdentityServer4/IdentityServer4/AbpZeroIdentityServerBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Abp.Authorization.Users;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using Abp.Authorization.Users;
 using Abp.IdentityServer4;
 using Abp.Runtime.Security;
 using IdentityModel;
@@ -10,9 +12,12 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AbpZeroIdentityServerBuilderExtensions
     {
-        public static IIdentityServerBuilder AddAbpIdentityServer<TUser>(this IIdentityServerBuilder builder)
+        public static IIdentityServerBuilder AddAbpIdentityServer<TUser>(this IIdentityServerBuilder builder, Action<AbpIdentityServerOptions> optionsAction = null)
             where TUser : AbpUser<TUser>
         {
+            var options = new AbpIdentityServerOptions();
+            optionsAction?.Invoke(options);
+
             builder.AddAspNetIdentity<TUser>();
 
             builder.AddProfileService<AbpProfileService<TUser>>();
@@ -20,9 +25,19 @@ namespace Microsoft.Extensions.DependencyInjection
 
             builder.Services.Replace(ServiceDescriptor.Transient<IClaimsService, AbpClaimsService>());
 
-            AbpClaimTypes.UserId = JwtClaimTypes.Subject;
-            AbpClaimTypes.UserName = JwtClaimTypes.Name;
-            AbpClaimTypes.Role = JwtClaimTypes.Role;
+            if (options.UpdateAbpClaimTypes)
+            {
+                AbpClaimTypes.UserId = JwtClaimTypes.Subject;
+                AbpClaimTypes.UserName = JwtClaimTypes.Name;
+                AbpClaimTypes.Role = JwtClaimTypes.Role;
+            }
+
+            if (options.UpdateJwtSecurityTokenHandlerDefaultInboundClaimTypeMap)
+            {
+                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[AbpClaimTypes.UserId] = AbpClaimTypes.UserId;
+                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[AbpClaimTypes.UserName] = AbpClaimTypes.UserName;
+                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[AbpClaimTypes.Role] = AbpClaimTypes.Role;
+            }
 
             return builder;
         }
